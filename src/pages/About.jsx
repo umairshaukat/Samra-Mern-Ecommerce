f965eb4eda5dfbcc7b04d4caca7b58dcdd1143e5
umairshaukat <umairshaukat278@gmail.com>
2025-01-09 19:39:15 +0300

    More pages Created
    LoginPage, Contact page, About us Created

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Title from '../components/Title';
+import { assets } from '../assets/frontend_assets/assets';
+import Newsletter from '../components/Newsletter';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'About'} text2={'US'}/>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt=''/>
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <b className='text-gray-800'>Our Mission</b>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+      </div>
+
+      
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+        <b>Quality Assurance</b>
+        <p className='text-gray-600'>The quality assurance process helps a business ensure its products meet the quality standards set by the company or its industry. Another way to understand quality assurance (QA) is as a company's process for improving the quality of its products.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+        <b >Convenience</b>
+        <p className='text-gray-600'>Product that the customer usually buys frequently, immediately, and with a minimum of comparison and buying effort.” Convenience products usually have low prices, and to turn a decent profit, they have to be sold in large volumes.</p>
+        </div>
+
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+        <b>Exceptional Customer Service</b>
+        <p className='text-gray-600'>You can find other ways to welcome new customers if a hand-written note doesn't fit. You might send a personalized welcome email, call a new customer or record a brief video welcome for each client.</p>
+        </div>
+      </div>
+      <Newsletter/>
+    </div>
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets'
+import Newsletter from '../components/Newsletter'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} text2={'US'}/>
+      </div>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img src={assets.contact_img} className='w-full md:max-w-[480px]' alt=''/>
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'>OUR STORE</p>
+          <p className='text-gray-500'>54709 Wilams Station <br/> Suite 350, Washington, USA</p>
+          <p className='text-gray-500'>Tel: (415) 555-5479<br/> Email: admin@samra.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at SAMRA</p>
+          <p className='text-gray-500'>Learn More about our teams and job openings</p>
+          <button className='border border-black px-8 py-4 text-sm hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+      <Newsletter/>
+    </div>
   )
 }
 
